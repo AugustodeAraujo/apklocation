4db5c917f3878e7e875ceb67eb7800c9fa6ff01f
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-07-22 11:28:53 -0300

    APK Test

---
+++ src/views/home/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#009933',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    textpecista:{
+        color: 'white', 
+    }
+});
+
+export default styles; 
